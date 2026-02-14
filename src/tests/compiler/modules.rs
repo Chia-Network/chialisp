cb03f8b41fa9869630737f53b25f68e0b5426652
arty <art.yerkes@gmail.com>
2026-02-13 16:01:24 -0800

    fmt

--- src/tests/compiler/modules.rs
+++ src/tests/compiler/modules.rs
@@ -799,7 +799,10 @@ fn test_module_cache_dep_changed() {
     // one for which cache also exists.
     source_opts.erase_written("resources/tests/module/programs/two-outputs_F.hex");
     // File erased.  If we can set it later, it's because compiling traversed and rewrote it.
-    assert_eq!(source_opts.get_written_file("resources/tests/module/programs/two-outputs_F.hex"), None);
+    assert_eq!(
+        source_opts.get_written_file("resources/tests/module/programs/two-outputs_F.hex"),
+        None
+    );
 
     let source_opts = test_compile_and_run_program_with_modules_and_fs(
         source_opts,
@@ -810,10 +813,14 @@ fn test_module_cache_dep_changed() {
             argument: initial_program,
             outcome: ContentEquals,
         }],
-    ).unwrap();
+    )
+    .unwrap();
 
     // The file should exist now.
-    assert_eq!(source_opts.get_written_file("resources/tests/module/programs/two-outputs_F.hex"), Some(b"ff02ffff01ff10ff05ffff010180ffff04ffff01ff10ff05ffff010180ff018080".to_vec()));
+    assert_eq!(
+        source_opts.get_written_file("resources/tests/module/programs/two-outputs_F.hex"),
+        Some(b"ff02ffff01ff10ff05ffff010180ffff04ffff01ff10ff05ffff010180ff018080".to_vec())
+    );
 }
 
 #[test]
