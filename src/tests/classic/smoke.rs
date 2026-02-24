296452033e9d79f3f91a9d55e8cfeb4b468c5315
Adam Kelly <338792+aqk@users.noreply.github.com>
2026-02-23 16:17:03 -0800

    use new function signatures for call_tool and launch_tool

--- src/tests/classic/smoke.rs
+++ src/tests/classic/smoke.rs
@@ -281,8 +281,10 @@ fn binutils_assemble() {
 #[test]
 fn quoted_negative() {
     let mut s = Stream::new(None);
+    let mut serr = Stream::new(None);
     launch_tool(
         &mut s,
+        &mut serr,
         &vec!["run".to_string(), "-d".to_string(), "(q . -3)".to_string()],
         &"run".to_string(),
         2,
@@ -432,8 +434,10 @@ fn test_simple_opd_conversion() {
 #[test]
 fn test_simple_brun_minus_x_1() {
     let mut s = Stream::new(None);
+    let mut serr = Stream::new(None);
     launch_tool(
         &mut s,
+        &mut serr,
         &vec![
             "brun".to_string(),
             "-x".to_string(),
@@ -450,8 +454,10 @@ fn test_simple_brun_minus_x_1() {
 #[test]
 fn test_simple_brun_minus_x_2() {
     let mut s = Stream::new(None);
+    let mut serr = Stream::new(None);
     launch_tool(
         &mut s,
+        &mut serr,
         &vec![
             "brun".to_string(),
             "-x".to_string(),
@@ -649,8 +655,10 @@ fn pool_member_innerpuz() {
     "};
     let desired = "(a (q 2 (i 767 (q 2 22 (c 2 (c 5 (c 1215 (c 1727 (c 383 (c (sha256 (logior 47 (logand (q . 0x00ffffffffffffffffffffffffffffffff) 767)) 11 383) ()))))))) (q 4 (c 8 (c 23 (c (a 30 (c 2 (c 383 ()))) ()))) (c (c 28 (c 95 (c 1727 ()))) ()))) 1) (c (q (50 61 . 51) 62 (c (c 28 (c 11 (c 23 ()))) (c (c 28 (c 5 (c 47 ()))) (c (c 10 (c 95 ())) (c (c 20 (c (sha256 95 (q . 36)) ())) ())))) 2 (i (l 5) (q 11 (q . 2) (a 30 (c 2 (c 9 ()))) (a 30 (c 2 (c 13 ())))) (q 11 (q . 1) 5)) 1) 1))".to_string();
     let mut s = Stream::new(None);
+    let mut serr = Stream::new(None);
     launch_tool(
         &mut s,
+        &mut serr,
         &vec![
             "run".to_string(),
             "--operators-version".to_string(),
@@ -673,16 +681,20 @@ fn test_non_consed_args() {
     let program =
         "(mod params (defun ident (arg) (f (list arg))) (ident (ident params)))".to_string();
     let mut s = Stream::new(None);
+    let mut serr = Stream::new(None);
     launch_tool(
         &mut s,
+        &mut serr,
         &vec!["run".to_string(), program],
         &"run".to_string(),
         2,
     );
     let result = s.get_value().decode().trim().to_string();
     let mut t = Stream::new(None);
+    let mut terr = Stream::new(None);
     launch_tool(
         &mut t,
+        &mut terr,
         &vec!["brun".to_string(), result, "99".to_string()],
         &"brun".to_string(),
         0,
@@ -837,8 +849,10 @@ fn test_sub_args() {
 #[test]
 fn test_smoke_cl23_program_with_zero_folding() {
     let mut s = Stream::new(None);
+    let mut serr = Stream::new(None);
     launch_tool(
         &mut s,
+        &mut serr,
         &vec![
             "run".to_string(),
             "(mod () (include *standard-cl-23*) (defconst X (concat 0x00 0x00)) X)".to_string(),
@@ -853,8 +867,10 @@ fn test_smoke_cl23_program_with_zero_folding() {
 #[test]
 fn test_smoke_cl23_program_without_zero_folding() {
     let mut s = Stream::new(None);
+    let mut serr = Stream::new(None);
     launch_tool(
         &mut s,
+        &mut serr,
         &vec![
             "run".to_string(),
             "(mod () (include *standard-cl-23.1*) (defconst X (concat 0x00 0x00)) X)".to_string(),
