8b7bb0d8c9c8d7954541cc85041ec8b54a095800
art yerkes <art.yerkes@gmail.com>
2026-04-09 13:27:40 -0700

    accidental change

--- resources/tests/test_sdc.sh
+++ resources/tests/test_sdc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./target/debug/armtx -o tsd.elf ./resources/tests/simple_deinline_case_23.clsp '(5)'
+./target/debug/armtx -o sdc.elf ./resources/tests/simple_deinline_case_23.clsp '(5)'
