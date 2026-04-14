c3869b8647e70d949c8b2ea9cb7c652113a188b6
arty <art.yerkes@gmail.com>
2026-04-13 20:28:36 -0700

    try localhost explicitly

--- resources/tests/test_sdc_gdb.sh
+++ resources/tests/test_sdc_gdb.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gdb-multiarch --ex "set confirm off" --ex "file sdc.elf" --ex "source support/gdb_print_sexp.py" --ex "handle SIGUSR1 noprint nostop pass" --ex "dir resources/tests" --ex "target remote :9001" --ex "continue" --ex "quit 0" 2>&1
+gdb-multiarch --ex "set confirm off" --ex "file sdc.elf" --ex "source support/gdb_print_sexp.py" --ex "handle SIGUSR1 noprint nostop pass" --ex "dir resources/tests" --ex "target remote 127.0.0.1:9001" --ex "continue" --ex "quit 0" 2>&1
