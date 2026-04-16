9c4af974ec69e5fb70244eca0a1bb2936e4e0613
Adam Kelly <adam@chia.net>
2026-04-15 22:14:34 -0400

    Use an array of zero bytes instead of nil to represent "no fingerprint" Doing this allows concatenation of an ordered set of signatures to uniquely represent that set.

--- src/compiler/compiler.rs
+++ src/compiler/compiler.rs
@@ -630,7 +630,9 @@ fn add_main_fingerprint(cf: &mut CompileForm, forms: &[Rc<SExp>]) {
         nl: cf.loc(),
         name: b"main".to_vec(),
         kind: Some(IncludeProcessType::Compiled),
-        fingerprint: sha256tree(form_list),
+        fingerprint: sha256tree(form_list)
+            .try_into()
+            .expect("sha256tree digest is 32 bytes"),
     });
 }
 
